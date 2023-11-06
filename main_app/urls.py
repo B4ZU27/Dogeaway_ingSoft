7615767e0ed3fb3386143e47a41db007ba203344
Z4v4l4 <ulisesbz27@gamil.com>
2023-11-06 12:06:59 -0600

    super user en el admin

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import ListaUsuariosView
 
 urlpatterns = [
-    path('', views.index),
+    path('home/', views.index),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('registro_mascota/', views.registro_mascota, name='registro_mascota'),
