22328178a34053909ff7488deef2e72b0a68825f
Ulises <ulises.barajas1297@alumnos.udg.mx>
2023-11-23 13:35:56 -0600

    subi un error, esto lo arregla el error, sorry

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import AbstractUser #Importa el modelo base de USUARIO de DJANGO
 import json
 
